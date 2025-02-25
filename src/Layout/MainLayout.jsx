56c6152a04c90f5c1aac5a2b8949097adab95248
MahdiHassanGo <contact.mahdiasif@gmail.com>
2025-02-25 12:32:43 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../components/Login";
+
+function MainLayout() {
+  return (
+    <div>
+      <div>
+        <Login></Login>
+      </div>
+    </div>
+  );
+}
+
+export default MainLayout;
---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FaGoogle } from "react-icons/fa";
+function Login() {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold text-center">Please Login First</h1>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+              <div className="mt-5">
+                <button className="btn rounded-full">
+                  Google Sign in <FaGoogle></FaGoogle>
+                </button>
+              </div>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
